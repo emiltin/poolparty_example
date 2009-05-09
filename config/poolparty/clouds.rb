797bf5dffc535489af2ae6d2bc4a9d16c4206073
Emil Tin <emil@tin.dk>
2009-05-09 13:10:08 +0200

    manaully creating mysql db to avoid has_rails_deploy failing

--- config/poolparty/clouds.rb
+++ config/poolparty/clouds.rb
@@ -12,6 +12,17 @@ pool :application do
 
     has_file "/etc/motd", :content => "Welcome!"                    #login message (message-of-the-day)
     
+    
+    
+    #FIXME:
+    #has_rails_deploy is currently (1.2) broken if we're using mysql. the problem is that the migration fails because
+    #the db hasn't been created yet. a worksaround could have been to use:
+    #   migration_command "rake db:create && RAILS_ENV=production rake db:migrate"
+    #but rails_deploy doesn't currently pass on the migration_command to chef_deploy, so for the time being, 
+    #we create the db manually
+    has_exec(:name => "Create mysql db", :command => "mysql -e 'create database if not exists my_app;'")
+    
+    
     has_rails_deploy "my_app" do                                    #deploy our rails app using apache + mod_rails/passenger  
       dir "/var/www"
       repo "git://github.com/emiltin/poolparty_example.git"         #download rails app from this repo
@@ -19,8 +30,6 @@ pool :application do
       database_yml "#{File.dirname(__FILE__)}/../database.yml"      #will copy it to the shared folder
       
       
-      #the problem with this is that rails_deploy doesn't currently pass on the migration_command to chef_deploy...
-      migration_command "rake db:create && RAILS_ENV=production rake db:migrate"
     end
 
     chef do
